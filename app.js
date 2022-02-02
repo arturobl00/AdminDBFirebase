84f298c32bf7488ff3548560462c2a77e72d5656
arturobl00 <arturobl00@msn.com>
2022-02-02 14:15:45 -0600

    Primera Practica

---
+++ app.js
@@ -0,0 +1,9 @@
+function ingresar(){
+    console.log("Esto funciona")
+
+    var email = document.getElementById('email').value;
+    var pass = document.getElementById('pass').value;
+
+    console.log(email," Enviado")
+    console.log(pass, " Enviado")
+}
