09d0354936ede5c1b53c3f1c203625519ae640c8
arturobl00 <arturobl00@msn.com>
2022-02-08 16:04:50 -0600

    semana2 auth

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-function ingresar(){
-    console.log("Esto funciona")
+function registrar(){
+    //console.log("Esto funciona")
 
     var email = document.getElementById('email').value;
     var pass = document.getElementById('pass').value;
 
-    console.log(email," Enviado")
-    console.log(pass, " Enviado")
+    firebase.auth().createUserWithEmailAndPassword(email, pass)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    console.log("Funciono")
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode," Codigo de Error")
+    console.log(errorMessage)
+    // ..
+  });
 }
